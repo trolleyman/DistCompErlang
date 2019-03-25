3917b1768150d990f8d7e05864f826b3aac4afd6
Callum Tolley <cgtrolley@gmail.com>
2019-03-25 04:16:03 +0000

    Added report, options

---
+++ report.md
@@ -0,0 +1,156 @@
+
+# Distributed and Parallel (Extended) - Assignment 2
+## Group 10
+Mason Cusack (1414624),
+Vadim Makarenkov (1566767),
+Callum Tolley (1552710), and
+David Somers (1567268)
+
+We developed a program that achieves all the goals set in the assignment.
+
+It takes two optional arguments:<br/>
+`--verbose`: reports more output about the algorithm<br/>
+`--breadth-first`: performs the breadth-first algorithm, where the token is sent through each link twice.
+
+The code has been simplified from it's original form.
+
+The initiator and normal nodes were different functions originally. Now, there is one
+entry function, `init_node`, which waits for the initial broadcast of neighbour PIDs,
+and a recursive main node function, `node`.
+
+We combined the node functions as they shared much of the same code: now the only
+difference is that an initiator node starts out with a parent, wheras a normal node
+doesn't (i.e. it starts out with the special atom `none`). This is possible because
+the parent is only set on a node if it doesn't have a parent already.
+
+It was decided that every message would be of a uniform format: it would be a list, where
+the first element was always an `atom` that formed the ID of the message, and the rest
+were arguments.
+
+Originally, there were four types of messages, but now there are only three:
+| ID | Args | Notes |
+|----|------|-------|
+| `neighbour_ids` | `[Neighbours]` | Sent initially to inform nodes about the PIDs and names of it's neighbours. `Neighbours` is an array of tuples of the form `{Pid, Name}`.
+| `token` | `[Token, From]` | Main message used in the algorithm to distribute the token around. `Token` is the token, but reversed for efficiency's sake (prepending is more efficient than appending in Erlang). `From` contains which PID the message is from.
+| `exit` | *N/A* | Signals the node to exit.
+
+
+### Sample Output
+#### Normal output
+```console
+$ escript tarry.erl < input.txt
+p q t r t s u s t q p
+```
+
+#### Breadth first (non-extended part)
+```console
+$ escript tarry.erl --breadth-first < input.txt
+p s t q s q p q r t u s u t r q t s p
+```
+
+#### `input_matrix.txt`
+`input_matrix.txt` is a 3x3 matrix of nodes labelled a-i, with adjacent nodes (including diagonals) linked
+```console
+$ escript tarry.erl < input_matrix.txt
+e a d h f i f b c b f h g h d a e
+$ escript tarry.erl --breadth-first < input_matrix.txt
+e g h f c b e c e a d g d e d b f e b d h i f b a b c f i e i h d a e h e f h g e
+```
+
+#### Depth first (Verbose)
+```console
+$ escript tarry.erl --verbose < input.txt
+Nodes: [{"p",["q","s"]},
+        {"q",["p","s","t","r"]},
+        {"r",["q","t"]},
+        {"s",["p","q","t","u"]},
+        {"t",["q","r","u","s"]},
+        {"u",["s","t"]}]
+Initiator: "p"
+<0.32.0> p: Waiting for neighbour IDs...
+<0.33.0> q: Waiting for neighbour IDs...
+<0.34.0> r: Waiting for neighbour IDs...
+<0.35.0> s: Waiting for neighbour IDs...
+<0.36.0> t: Waiting for neighbour IDs...
+<0.37.0> u: Waiting for neighbour IDs...
+<0.32.0> p: Received neighbours: q s
+<0.33.0> q: Received neighbours: p s t r
+<0.34.0> r: Received neighbours: q t
+<0.35.0> s: Received neighbours: p q t u
+<0.36.0> t: Received neighbours: q r u s
+<0.37.0> u: Received neighbours: s t
+<0.32.0> p: remaining neighbours: [{<0.35.0>,"s"}]
+<0.32.0> p: got token, sending to neighbour q (<0.33.0>):
+<0.33.0> q: remaining neighbours: [{<0.36.0>,"t"},{<0.35.0>,"s"}]
+<0.33.0> q: got token, sending to neighbour r (<0.34.0>): p
+<0.34.0> r: remaining neighbours: []
+<0.34.0> r: got token, sending to neighbour t (<0.36.0>): q p
+<0.36.0> t: remaining neighbours: [{<0.37.0>,"u"}]
+<0.36.0> t: got token, sending to neighbour s (<0.35.0>): r q p
+<0.35.0> s: remaining neighbours: []
+<0.35.0> s: got token, sending to neighbour u (<0.37.0>): t r q p
+<0.37.0> u: no neighbours, returning token to parent (<0.35.0>): s t r q p
+<0.35.0> s: no neighbours, returning token to parent (<0.36.0>): u s t r q p
+<0.36.0> t: no neighbours, returning token to parent (<0.34.0>): s u s t r q p
+<0.34.0> r: no neighbours, returning token to parent (<0.33.0>): t s u s t r q p
+<0.33.0> q: no neighbours, returning token to parent (<0.32.0>): r t s u s t r q p
+<0.32.0> p: no neighbours, returning token to parent (<0.2.0>): q r t s u s t r q p
+p q r t s u s t r q p
+```
+
+#### Breadth first (Verbose)
+```console
+$ escript tarry.erl --breadth-first --verbose < input.txt
+Nodes: [{"p",["q","s"]},
+        {"q",["p","s","t","r"]},
+        {"r",["q","t"]},
+        {"s",["p","q","t","u"]},
+        {"t",["q","r","u","s"]},
+        {"u",["s","t"]}]
+Initiator: "p"
+<0.32.0> p: Waiting for neighbour IDs...
+<0.33.0> q: Waiting for neighbour IDs...
+<0.34.0> r: Waiting for neighbour IDs...
+<0.35.0> s: Waiting for neighbour IDs...
+<0.36.0> t: Waiting for neighbour IDs...
+<0.37.0> u: Waiting for neighbour IDs...
+<0.32.0> p: Received neighbours: q s
+<0.33.0> q: Received neighbours: p s t r
+<0.34.0> r: Received neighbours: q t
+<0.35.0> s: Received neighbours: p q t u
+<0.36.0> t: Received neighbours: q r u s
+<0.37.0> u: Received neighbours: s t
+<0.32.0> p: remaining neighbours: [{<0.33.0>,"q"}]
+<0.32.0> p: got token, sending to neighbour s (<0.35.0>):
+<0.35.0> s: remaining neighbours: [{<0.33.0>,"q"},{<0.37.0>,"u"}]
+<0.35.0> s: got token, sending to neighbour t (<0.36.0>): p
+<0.36.0> t: remaining neighbours: [{<0.37.0>,"u"},{<0.34.0>,"r"}]
+<0.36.0> t: got token, sending to neighbour q (<0.33.0>): s p
+<0.33.0> q: remaining neighbours: [{<0.32.0>,"p"},{<0.34.0>,"r"}]
+<0.33.0> q: got token, sending to neighbour s (<0.35.0>): t s p
+<0.35.0> s: remaining neighbours: [{<0.37.0>,"u"}]
+<0.35.0> s: got token, sending to neighbour q (<0.33.0>): q t s p
+<0.33.0> q: remaining neighbours: [{<0.34.0>,"r"}]
+<0.33.0> q: got token, sending to neighbour p (<0.32.0>): s q t s p
+<0.32.0> p: remaining neighbours: []
+<0.32.0> p: got token, sending to neighbour q (<0.33.0>): q s q t s p
+<0.33.0> q: remaining neighbours: []
+<0.33.0> q: got token, sending to neighbour r (<0.34.0>): p q s q t s p
+<0.34.0> r: remaining neighbours: []
+<0.34.0> r: got token, sending to neighbour t (<0.36.0>): q p q s q t s p
+<0.36.0> t: remaining neighbours: [{<0.34.0>,"r"}]
+<0.36.0> t: got token, sending to neighbour u (<0.37.0>): r q p q s q t s p
+<0.37.0> u: remaining neighbours: []
+<0.37.0> u: got token, sending to neighbour s (<0.35.0>): t r q p q s q t s p
+<0.35.0> s: remaining neighbours: []
+<0.35.0> s: got token, sending to neighbour u (<0.37.0>): u t r q p q s q t s p
+<0.37.0> u: no neighbours, returning token to parent (<0.36.0>): s u t r q p q s q t s p
+<0.36.0> t: remaining neighbours: []
+<0.36.0> t: got token, sending to neighbour r (<0.34.0>): u s u t r q p q s q t s p
+<0.34.0> r: no neighbours, returning token to parent (<0.33.0>): t u s u t r q p q s q t s p
+<0.33.0> q: no neighbours, returning token to parent (<0.36.0>): r t u s u t r q p q s q t s p
+<0.36.0> t: no neighbours, returning token to parent (<0.35.0>): q r t u s u t r q p q s q t s p
+<0.35.0> s: no neighbours, returning token to parent (<0.32.0>): t q r t u s u t r q p q s q t s p
+<0.32.0> p: no neighbours, returning token to parent (<0.2.0>): s t q r t u s u t r q p q s q t s p
+p s t q s q p q r t u s u t r q t s p
+```
--- tarry.erl
+++ tarry.erl
@@ -1,52 +1,23 @@
 % command to run: cat input.txt | escript tarry.erl
 
 -module(tarry).
--export([main/0, main/1, init_node/1]).
+-export([main/0, main/1, init_node/3]).
 
 % Needed, to disable interpreted mode, to allow us to reference functions
 % below others in the file
 -mode(compile).
 
--ifdef(debug).
--define(DEBUG(Format, Args), io:format(Format, Args)).
--else.
--define(DEBUG(Format, Args), id(Format), id(Args)).
--endif.
-
-main() ->
-  main([]).
-
-main([]) ->
-  % Read and tokenize input
-  {InitiatorName, Nodes} = get_nodes(),
-
-  ?DEBUG("Nodes: ~p~n", [Nodes]),
-  ?DEBUG("Initiator: ~p~n", [InitiatorName]),
-
-  % Spawn nodes
-  {InitiatorId, AllIds} = spawn_nodes(Nodes, InitiatorName),
-
-  % Initiate tarry
-  InitiatorId ! [token, [], self()],
-
-  receive [token, Token, _] ->
-    io:format("~s~n", [fmt_token(lists:reverse(Token))])
-  end,
-
-  % Tell nodes to exit
-  lists:foreach(fun (Id) -> Id ! [exit] end, AllIds);
-
-
-main(_) ->
-  halt(1).
-
 % == Utils ==
+fdebug(true, Format, Args) ->
+  io:format(Format, Args);
+fdebug(false, _, _) ->
+  ok.
+
 shuffle_list(List) ->
   {MegaSecs, Secs, MicroSecs} = now(),
   Hash = crypto:hash(md5, pid_to_list(self())),
   Seed = {MegaSecs + binary:at(Hash, 0), Secs + binary:at(Hash, 1), MicroSecs + binary:at(Hash, 2)},
   random:seed(Seed),
-  ?DEBUG("Seed random: ~p~n", [Seed]),
   [X||{_,X} <- lists:sort([ {random:uniform(), N} || N <- List])].
 
 list_contains([], _) -> false;
@@ -62,58 +33,98 @@ fmt_token(Token) ->
 fmt_nbours(NBours) ->
   string:join([Name || {_, Name} <- NBours], " ").
 
-id(X) -> X.
-
 % == Main functions ==
-% Get nodes from stdin
-get_nodes() -> 
-  [{InititatorName, _}|Nodes] = get_all_nodes([]),
+main() ->
+  main([]).
+
+main(Args) ->
+  IsVerbose = list_contains(Args, "--verbose"),
+  IsDepthFirst = not list_contains(Args, "--breadth-first"),
+
+  % Read and tokenize input
+  {InitiatorName, NodeSpec} = get_node_spec(),
+
+  fdebug(IsVerbose, "Nodes: ~p~n", [NodeSpec]),
+  fdebug(IsVerbose, "Initiator: ~p~n", [InitiatorName]),
+
+  % Spawn nodes
+  {InitiatorId, AllIds} = spawn_nodes(IsVerbose, IsDepthFirst, NodeSpec, InitiatorName),
+
+  % Initiate tarry
+  Token = run_tarry(InitiatorId, AllIds),
+  
+  io:format("~s~n", [fmt_token(Token)]).
+
+% Get node spec from stdin
+get_node_spec() -> 
+  [{InititatorName, _}|Nodes] = get_node_spec([]),
   {InititatorName, Nodes}.
 
-get_all_nodes(Acc) ->
+get_node_spec(Acc) ->
   case io:get_line("") of
     eof ->
       lists:reverse(Acc);
     Line ->
       case string:tokens(Line, " \t\n\r\n") of
-        [] -> get_all_nodes(Acc);
-        [Name | Neighbours] -> get_all_nodes([{Name, Neighbours}|Acc])
+        [] -> get_node_spec(Acc);
+        [Name | Neighbours] -> get_node_spec([{Name, Neighbours}|Acc])
       end
   end.
 
 % Spawn nodes, and return initiator ID
-spawn_nodes(NodeSpec, InitiatorName) ->
+spawn_nodes(IsVerbose, IsDepthFirst, NodeSpec, InitiatorName) ->
   % Spawn nodes, and keep PIDs in a list
-  NodePids = [spawn(?MODULE, init_node, [Name]) || {Name, _} <- NodeSpec],
+  NodePids = [spawn(?MODULE, init_node, [IsVerbose, IsDepthFirst, Name]) || {Name, _} <- NodeSpec],
 
-  % Combine node PIDs into list
+  % Combine node PIDs into list of tuples {Pid, Name, NeighbourNames}
   {NodeNames, NodeNeighbours} = lists:unzip(NodeSpec),
   Nodes = lists:zip3(NodePids, NodeNames, NodeNeighbours),
 
+   % GetPid gets the PID of the node with the specified name, and returns a tuple of that PID, and the name passed.
+  GetPid = fun (Name) -> case lists:keyfind(Name, 2, Nodes) of {Pid, _, _} -> {Pid, Name} end end,
+
   % Send neighbour info to all nodes
-  GetNeighbours = fun (Name) -> case lists:keyfind(Name, 2, Nodes) of {Pid, _, _} -> {Pid, Name} end end,
-  lists:foreach(fun ({Pid, _, NeighbourNames}) -> Pid ! [neighbour_ids, lists:map(GetNeighbours, NeighbourNames)] end, Nodes),
+  lists:foreach(fun ({Pid, _, NeighbourNames}) -> Pid ! [neighbour_ids, lists:map(GetPid, NeighbourNames)] end, Nodes),
 
   % Get initiator PID, and return
   {InitiatorPid, _, _} = lists:keyfind(InitiatorName, 2, Nodes),
+
+  % Get list of all PIDs, so that we know which nodes to send exit message to
   AllPids = [Pid || {Pid, _, _} <- Nodes],
   {InitiatorPid, AllPids}.
 
+% Run tarry algorithm, once all the nodes are set up correctly.
+% Also, clean up the nodes once the algorithm is complete.
+% Send initiation token
+run_tarry(InitiatorId, AllIds) ->
+  % Send start token
+  InitiatorId ! [token, [], self()],
+
+  % Receive token
+  receive [token, RevToken, _] ->
+    % Clean up nodes
+    lists:foreach(fun (Id) -> Id ! [exit] end, AllIds),
+
+    % Reverse token so that it is in the correct direction,
+    % and return it. We do this so that it is more efficient
+    % when appending to the token throughout the tarry
+    lists:reverse(RevToken)
+  end.
 
 % Run node - waits for neighbour ids
-init_node(Name) ->
-  ?DEBUG("~p ~s: Waiting for neighbour IDs...~n", [self(), Name]),
+init_node(IsVerbose, IsDepthFirst, Name) ->
+  fdebug(IsVerbose, "~p ~s: Waiting for neighbour IDs...~n", [self(), Name]),
   receive
     [neighbour_ids, Neighbours] ->
-      ?DEBUG("~p ~s: Received neighbours: ~s~n", [self(), Name, fmt_nbours(Neighbours)]),
+      fdebug(IsVerbose, "~p ~s: Received neighbours: ~s~n", [self(), Name, fmt_nbours(Neighbours)]),
       RandomNeighbours = shuffle_list(Neighbours),
-      node(Name, RandomNeighbours, none);
+      node(IsVerbose, IsDepthFirst, Name, RandomNeighbours, none);
     Msg ->
-      ?DEBUG("~p ~s: Unknown message: ~p~n", [self(), Name, Msg])
+      fdebug(IsVerbose, "~p ~s: Unknown message: ~p~n", [self(), Name, Msg])
   end.
 
 % Run node
-node(Name, Neighbours, OldParentId) ->
+node(IsVerbose, IsDepthFirst, Name, Neighbours, OldParentId) ->
   receive
     [exit] -> ok;
     [token, Token, From] ->
@@ -122,20 +133,23 @@ node(Name, Neighbours, OldParentId) ->
         none -> From;
         _ -> OldParentId
       end,
-      FilteredNeighbours = [Neighbour || {Pid, NName} = Neighbour <- Neighbours, Pid =/= ParentId, not list_contains(Token, NName)],
+      FilteredNeighbours = case IsDepthFirst of
+        true  -> [Neighbour || {Pid, NName} = Neighbour <- Neighbours, Pid =/= ParentId, not list_contains(Token, NName)];
+        false -> [Neighbour || {Pid, _    } = Neighbour <- Neighbours, Pid =/= ParentId]
+      end,
       case FilteredNeighbours of
         [] ->
           % No remaining neighbours - return to parent
-          ?DEBUG("~p ~s: no neighbours, returning token to parent (~p): ~s~n", [self(), Name, ParentId, fmt_token(Token)]),
+          fdebug(IsVerbose, "~p ~s: no neighbours, returning token to parent (~p): ~s~n", [self(), Name, ParentId, fmt_token(Token)]),
           ParentId ! [token, [Name|Token], self()],
-          node(Name, [], ParentId);
+          node(IsVerbose, IsDepthFirst, Name, [], ParentId);
         [{NPid, NName} | RemainingNeighbours] ->
-          ?DEBUG("~p ~s: remaining neighbours: ~p~n", [self(), Name, RemainingNeighbours]),
-          ?DEBUG("~p ~s: got token, sending to neighbour ~s (~p): ~s~n", [self(), Name, NName, NPid, fmt_token(Token)]),
+          fdebug(IsVerbose, "~p ~s: remaining neighbours: ~p~n", [self(), Name, RemainingNeighbours]),
+          fdebug(IsVerbose, "~p ~s: got token, sending to neighbour ~s (~p): ~s~n", [self(), Name, NName, NPid, fmt_token(Token)]),
           NPid ! [token, [Name|Token], self()],
-          node(Name, RemainingNeighbours, ParentId)
+          node(IsVerbose, IsDepthFirst, Name, RemainingNeighbours, ParentId)
       end;
     Msg ->
-      ?DEBUG("~p ~s: Unknown message: ~p~n", [self(), Name, Msg])
+      fdebug(IsVerbose, "~p ~s: Unknown message: ~p~n", [self(), Name, Msg])
   end.
 
